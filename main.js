41330805e8a0708d35560a6a34cf261a739a6fde
Dano <kastaramation@gmail.com>
2021-05-29 19:59:33 +0300

    seperated main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const express = require("express");
 require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const connectDB = require("./database/db");
-const cors = require("cors");
 const indexRouter = require("./routes/router");
+// Connecting to Database
 connectDB();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
 app.use('/api',indexRouter)
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}.`);

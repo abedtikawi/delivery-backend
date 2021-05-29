6f499dafdad5e67ddc25254363c31caa1f622abe
Dano <kastaramation@gmail.com>
2021-05-30 00:00:28 +0300

    added comments and body params

--- controllers/insertClient.js
+++ controllers/insertClient.js
@@ -1,25 +1,34 @@
 const Clients = require("../models/clients");
 const bcrypt = require("bcrypt");
 const validateBody = require("../utils/validateBody");
+/** Body params
+ * @param fname
+ * @param lname
+ * @param email
+ * @param password
+ */
 module.exports = async (req, res) => {
   console.log("[+] Checking request for empty fields in InsertClient");
+  // Passing the request with utils.validateBody for validation errors
   if (validateBody(req, res)) return;
 
   try {
     const { fname, lname, email } = req.body;
     console.log("[+] Generating salt and encrypting Password");
+    // Generating Salt=10
     const salt = await bcrypt.genSalt(10);
+    // Encrypting password
     let encryptedPassword = await bcrypt.hash(req.body.password, salt);
-
     console.log("[+] Creating Client ...");
+    // Creating a client object and inserting into Database
     let client = await Clients.create({
       fname,
       lname,
       email,
       password: encryptedPassword,
     });
-    console.log(`[+] Successfully created Client ${email}`);
 
+    console.log(`[+] Successfully created Client ${email}`);
     return res.status(200).json({ msg: "Success", api: client });
   } catch (error) {
     console.log("[-] An error occured in InsertClient.js");
--- middleware/validateItem.js
+++ middleware/validateItem.js
@@ -3,6 +3,5 @@ const { check } = require("express-validator");
 module.exports = [
   check("itemName").not().isEmpty().withMessage("Empty itemName"),
   check("price").not().isEmpty().withMessage("Empty price"),
-  check("location").not().isEmpty().withMessage("Empty location"),
-
+  check("quantity").not().isEmpty().withMessage("Empty quantity"),
 ];
